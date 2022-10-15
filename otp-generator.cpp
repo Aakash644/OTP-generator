6950d4786ea45c00604edff5f93929ab12cb3e4d
Aakash <Aakash.21BT@dpgitm.ac.in>
2022-10-15 15:48:34 +0530

    initial commit

---
+++ otp-generator.cpp
@@ -0,0 +1,55 @@
+#include <iostream> 
+#include<windows.h>
+#include<cstdlib> 
+#include<time.h>   
+#include<cstring>
+
+using namespace std;  
+
+
+string otpgenerator( int digits){ 
+    string otp;
+    string str="0123456789";
+    int k=str.length(); 
+    srand(time(0)); 
+    for(int i=0;i<digits;i++){ 
+       int j=rand()%k;
+       otp[i]=str[j];
+    }
+   for(int j=0;j<digits;j++){
+ cout<<otp[j];
+    
+    } 
+    return otp;
+}
+   
+int main() {
+top:  
+system("cls");
+int choice,n;
+  printf("**OTP Generator**\n"); 
+ printf("Enter your choice:\n");   
+  printf("1.OTP\n");   
+   printf("2.Exit\n");  
+cin>>choice;
+
+   
+    if(choice==1){ 
+        printf("Enter the number of digits in your OTP:\n");  
+        cin>>n;
+        cout<<"Your OTP is:\n";
+        cout<<otpgenerator(n)<<endl; 
+        printf("OTP will expire in 10 seconds.....\n");  
+        Sleep(10000);              //time in milliseconds
+         printf("OTP expired.....\n");   
+         Sleep(5000);
+        goto top;                  // label to return to main menu
+                
+    }  
+    else if(choice==2){
+        exit(0);
+    }
+
+
+    return 0;
+}
